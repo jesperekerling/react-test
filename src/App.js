175e895deeb0017a8b762758ce9707dd1acf59a6
jesperekerling <jesper@forme.se>
2023-09-07 13:22:43 +0200

    CarInfo added, test of creating component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import CarInfo from './CarInfo'
 
 function App() {
+  const a = 4+4;
+  const brand = "Audi";
+  const modelYear = 2000;
+  const model = "A3";
+  const currentMileage = 7410;
+  const bigServiceMileage = 10000;
+
+  let message = `You own a ${brand} ${model} that has been driven for ${currentMileage} km`
+  console.log(message);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <CarInfo />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hej {a}</p>
+        <p>I have an {brand} {model}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/CarInfo.js
@@ -0,0 +1,8 @@
+function CarInfo () {
+    return (
+        <div>
+            <h1>Hej från CarInfo</h1>
+        </div>
+    )
+}
+export default CarInfo;
